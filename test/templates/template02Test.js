3e5e26911b2f79694877b35b16988214138d5e14
Zenedith <github@zenedith.mailgun.org>
2015-08-08 01:05:32 +0200

    Fixed tests

--- test/templates/template02Test.js
+++ test/templates/template02Test.js
@@ -92,42 +92,44 @@ describe('template02 test', function () {
     ]
   };
 
-  var result = template02.handle(testData);
-  //console.dir(result, {depth: null});
-
   it('should import example', function (done) {
 
-    result.should.have.deep.property('events[0].name', 'Przystanek Woodstock sprzed lat');
-    result.should.have.deep.property('events[0].description', 'Zapraszamy na pokaz archiwalnych koncertów i materiałów przygotowanych przez TVP Gorzów Wielkopolski.');
-    result.should.have.deep.property('events[0].metadata[0]', '1734');
-    result.should.have.deep.property('events[0].metadata[1]', 'CDbyVRH2yt');
-    result.should.have.deep.property('events[0].duration.startAt', '2015-08-01T17:30:00.000Z');
-    result.should.have.deep.property('events[0].duration.finishAt', '2015-08-01T20:00:00.000Z');
-    result.should.have.deep.property('events[0].places[0].name', 'Warsztaty ASP');
-    result.should.have.deep.property('events[0].categories[0].name', 'Inne');
-    result.should.have.deep.property('events[0].categories[0].parent', null);
-    result.should.have.deep.property('events[0].images[0].url', 'http://files.parsetfss.com/9fd463ad-e07a-4a21-bdc6-283d608677ba/tfss-35c0b83c-b507-469e-aa1f-a39931906ee0-1658.jpg');
-    result.should.have.deep.property('events[0].tags[0]', 'film');
-    expect(result.events[0].authors).to.be.empty;
+    template02.handle(testData, function (err, result) {
+      //console.dir(result, {depth: null});
+      result.should.have.deep.property('events[0].name', 'Przystanek Woodstock sprzed lat');
+      result.should.have.deep.property('events[0].description', 'Zapraszamy na pokaz archiwalnych koncertów i materiałów przygotowanych przez TVP Gorzów Wielkopolski.');
+      result.should.have.deep.property('events[0].metadata[0]', '1734');
+      result.should.have.deep.property('events[0].metadata[1]', 'CDbyVRH2yt');
+      result.should.have.deep.property('events[0].duration.startAt', '2015-08-01T17:30:00.000Z');
+      result.should.have.deep.property('events[0].duration.finishAt', '2015-08-01T20:00:00.000Z');
+      result.should.have.deep.property('events[0].places[0].name', 'Warsztaty ASP');
+      result.should.have.deep.property('events[0].categories[0].name', 'Inne');
+      result.should.have.deep.property('events[0].categories[0].parent', null);
+      result.should.have.deep.property('events[0].images[0].url', 'http://files.parsetfss.com/9fd463ad-e07a-4a21-bdc6-283d608677ba/tfss-35c0b83c-b507-469e-aa1f-a39931906ee0-1658.jpg');
+      result.should.have.deep.property('events[0].tags[0]', 'film');
+      expect(result.events[0].authors).to.be.empty;
 
-    done();
+      done();
+    });
   });
 
   it('should import without image', function (done) {
-
-    result.should.have.deep.property('events[1].name', 'Spotkanie z zespołem Jamaram');
-    result.should.have.deep.property('events[1].description', 'Przystanek Woodstock to niezwykła energia krążąca między zespołami a festiwalową publicznością. W tym roku po raz kolejny Woodstockowicze będą mieć okazję z artystami grającymi na naszych scenach:  spotkać się, podpisach ich płyty czy po prostu powiedzieć, jak ważna jest ich muzyka.\n\nNamiot spotkań znajdziecie przy Małej Scenie. O każdym ze spotkań będziemy też przypominać na woodstockowym Twitterze (www.twitter.com/WoodstockPoland).');
-    result.should.have.deep.property('events[1].metadata[0]', '1735');
-    result.should.have.deep.property('events[1].metadata[1]', 'GfHGUTXG5s');
-    result.should.have.deep.property('events[1].duration.startAt', '2015-08-01T10:15:00.000Z');
-    result.should.have.deep.property('events[1].duration.finishAt', '2015-08-01T10:30:00.000Z');
-    result.should.have.deep.property('events[1].places[0].name', 'Mała Scena');
-    result.should.have.deep.property('events[1].categories[0].name', 'Inne');
-    result.should.have.deep.property('events[1].categories[0].parent', null);
-    result.should.have.deep.property('events[1].tags[0]', 'rock');
-    expect(result.events[1].images).to.be.empty;
-    expect(result.events[1].authors).to.be.empty;
-    done();
+    template02.handle(testData, function (err, result) {
+      //console.dir(result, {depth: null});
+      result.should.have.deep.property('events[1].name', 'Spotkanie z zespołem Jamaram');
+      result.should.have.deep.property('events[1].description', 'Przystanek Woodstock to niezwykła energia krążąca między zespołami a festiwalową publicznością. W tym roku po raz kolejny Woodstockowicze będą mieć okazję z artystami grającymi na naszych scenach:  spotkać się, podpisach ich płyty czy po prostu powiedzieć, jak ważna jest ich muzyka.\n\nNamiot spotkań znajdziecie przy Małej Scenie. O każdym ze spotkań będziemy też przypominać na woodstockowym Twitterze (www.twitter.com/WoodstockPoland).');
+      result.should.have.deep.property('events[1].metadata[0]', '1735');
+      result.should.have.deep.property('events[1].metadata[1]', 'GfHGUTXG5s');
+      result.should.have.deep.property('events[1].duration.startAt', '2015-08-01T10:15:00.000Z');
+      result.should.have.deep.property('events[1].duration.finishAt', '2015-08-01T10:30:00.000Z');
+      result.should.have.deep.property('events[1].places[0].name', 'Mała Scena');
+      result.should.have.deep.property('events[1].categories[0].name', 'Inne');
+      result.should.have.deep.property('events[1].categories[0].parent', null);
+      result.should.have.deep.property('events[1].tags[0]', 'rock');
+      expect(result.events[1].images).to.be.empty;
+      expect(result.events[1].authors).to.be.empty;
+      done();
+    });
   });
 
 });
